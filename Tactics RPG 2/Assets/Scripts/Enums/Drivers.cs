beac2b849845d7a0ff3bb9bfc9e533b24ddb3514
blee0730 <130094173+blee0730@users.noreply.github.com>
2026-01-21 18:06:35 -0700

    Delete Tactics RPG 2 directory

--- Tactics RPG 2/Assets/Scripts/Enums/Drivers.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public enum Drivers
-{
-	None,
-	Human,
-	Computer
-}
--- Tactics RPG 2/Assets/Scripts/View Model Component/Item/Merchandise.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Merchandise : MonoBehaviour
-{
-	public int buy;
-	public int sell;
-}
